38504c67c546981c096d8c87b9e3f4fd09992b48
Jennifer Earl <jennifer.earl@gmail.com>
2019-11-08 14:43:55 -0800

    starting to add database tables

--- app/pypi_clone_project/pypi_org/app.py
+++ app/pypi_clone_project/pypi_org/app.py
@@ -1,12 +1,16 @@
+import os
+
 import flask
 import pypi_org.services.package_service as package_service
 from pypi_org.infrastructure.view_modifiers import response
+import pypi_org.data.db_session as db_session
 
 app = flask.Flask(__name__)
 
 
 def main():
     register_blueprints()
+    setup_db()
     app.run(debug=True)
 
 
@@ -15,6 +19,15 @@ def register_blueprints():
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
     app.register_blueprint(cms_views.blueprint)
+
+
+def setup_db():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'pypi.sqlite')
+
+    db_session.global_init(db_file)
 
 
 if __name__ == '__main__':
---
+++ app/pypi_clone_project/pypi_org/data/__all_models.py
@@ -0,0 +1,4 @@
+# noinspection PyUnresolvedReferences
+from pypi_org.data.package import Package
+
+# all models go here
