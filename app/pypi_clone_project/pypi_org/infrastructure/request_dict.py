43c58f68fbd876a1d844b23d39153b47b3cd60d1
Jennifer Earl <jennifer.earl@gmail.com>
2019-12-20 15:58:56 -0800

    added viewmodels

--- app/pypi_clone_project/pypi_org/infrastructure/request_dict.py
+++ app/pypi_clone_project/pypi_org/infrastructure/request_dict.py
@@ -1,18 +1,36 @@
-from flask import Request
+import flask
+from werkzeug.datastructures import MultiDict
 
 
 class RequestDictionary(dict):
+    def __init__(self, *args, default_val=None, **kwargs):
+        self.default_val = default_val
+        super().__init__(*args, **kwargs)
+
     def __getattr__(self, key):
-        return self.get(key)
+        return self.get(key, self.default_val)
 
 
-def create(request: Request, **route_args) -> RequestDictionary:
+def create(default_val=None, **route_args) -> RequestDictionary:
+    request = flask.request
+
+    # Adding this retro actively. Some folks are experiencing issues where they
+    # are getting a list rather than plain dict. I think it's from multiple
+    # entries in the multidict. This should fix it.
+    args = request.args
+    if isinstance(request.args, MultiDict):
+        args = request.args.to_dict()
+
+    form = request.form
+    if isinstance(request.args, MultiDict):
+        form = request.form.to_dict()
+
     data = {
         # these args are ordered in least to highest priority, so, form posts override URL args, etc.
-        **request.args,     # the key/value pairs in the URL query string
+        **args,     # the key/value pairs in the URL query string
         **request.headers,  # Header values
-        **request.form,     # the key/value pairs in the body, from a form POST
+        **form,     # the key/value pairs in the body, from a form POST
         **route_args        # additional arguments the method access
     }
-    return RequestDictionary(data)
+    return RequestDictionary(data, default_val=default_val)
 
--- app/pypi_clone_project/pypi_org/views/account_views.py
+++ app/pypi_clone_project/pypi_org/views/account_views.py
@@ -1,8 +1,10 @@
 import flask
 
-from pypi_org.infrastructure import cookie_auth
+from pypi_org.infrastructure import cookie_auth, request_dict
 from pypi_org.infrastructure.view_modifiers import response
 from pypi_org.services import user_service
+from pypi_org.viewmodels.account.index_viewmodel import IndexViewModel
+from pypi_org.viewmodels.account.register_viewmodel import RegisterViewModel
 
 blueprint = flask.Blueprint('account', __name__, template_folder='templates')
 
@@ -10,53 +12,33 @@ blueprint = flask.Blueprint('account', __name__, template_folder='templates')
 @blueprint.route('/account')
 @response(template_file='account/index.html')
 def index():
-    user_id = cookie_auth.get_user_id_via_auth_cookie(flask.request)
-    if user_id is None:
+    vm = IndexViewModel()
+    if not vm.user:
         return flask.redirect('/account/login')
 
-    user = user_service.find_user_by_id(user_id)
-    if not user:
-        return flask.redirect('/account/login')
-
-    return {
-        'user': user,
-        'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
-    }
+    return vm.to_dict()
 
 
 @blueprint.route('/account/register', methods=['GET'])
 @response(template_file='account/register.html')
 def register_get():
-    return {
-        'user_id': cookie_auth.get_user_id_via_auth_cookie(flask.request),
-    }
+    vm = RegisterViewModel()
+    return vm.to_dict()
 
 
 @blueprint.route('/account/register', methods=['POST'])
 @response(template_file='account/register.html')
 def register_post():
+    vm = RegisterViewModel()
 
-    r = flask.request
-    name = r.form.get('name')
-    email = r.form.get('email', '').lower().strip()
-    password = r.form.get('password', '').strip()
+    vm.validate()
+    if vm.error:
+        return vm.to_dict()
 
-    if not name or not email or not password:
-        return {
-            'error': 'Some required fields are missing.',
-            'name': name,
-            'email': email,
-            'password': password
-        }
-
-    user = user_service.create_user(name, email, password)
+    user = user_service.create_user(vm.name, vm.email, vm.password)
     if not user:
-        return {
-            'error': 'A user with that email address already exists.',
-            'name': name,
-            'email': email,
-            'password': password
-        }
+        vm.error = 'The account could not be created.'
+        return vm.to_dict()
 
     resp = flask.redirect('/account')
     cookie_auth.set_auth(resp, user.id)
@@ -75,7 +57,6 @@ def login_get():
 @blueprint.route('/account/login', methods=['POST'])
 @response(template_file='account/login.html')
 def login_post():
-
     r = flask.request
     email = r.form.get('email', '').lower().strip()
     password = r.form.get('password', '').strip()
